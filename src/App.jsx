57e9eebd5623e79880e135719c73d9f166fa5fad
ezoltech <ezramamo13@gmail.com>
2024-11-08 20:45:21 -0300

    adding necessary folders 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="flex items-center justify-center">
-        <h2 className="font-extrabold">hello mama world</h2>
+        <h2 className="font-extrabold">hello juice world</h2>
       </div>
     </>
   );
