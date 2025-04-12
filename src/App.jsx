2fef1081a82c49fcd24c1125e2d979f0d81d342f
Nurhussen <nuryassinm132@gmail.com>
2025-04-13 02:04:10 +0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import "./index.css";
+import { motion } from "framer-motion";
 
 function App() {
   const location = useLocation();
@@ -79,6 +80,26 @@ function App() {
       <div className="network-status-message">
         {networkSpeedStatus && <p>{networkSpeedStatus}</p>}
       </div>
+      <iframe
+  src="https://www.mixweb.io/banner"
+  width="100%"
+  height="75px"
+  frameBorder="0"
+  style={{
+    border: "none",
+    display: "block",
+    position: "fixed",
+    top: "0",
+    zIndex: "9999",
+    backgroundColor: "white",
+  }}
+></iframe>
+<motion.header
+  initial={{ y: -50, opacity: 0 }}
+  animate={{ y: 0, opacity: 1 }}
+  transition={{ duration: 0.5 }}
+  className="mt-[75px] top-[75px] z-50 "
+></motion.header>
 
       <Outlet />
     </div>
